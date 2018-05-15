8f382a15c8a79eca0dcc225417b9b47ec57da5f6
Matthew <mateuszkrutel@wp.pl>
2018-05-15 18:41:05 +0200

    Added bills model, bills controller and index action

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Bill.create(name: "Kebab", price: 3000)
+Bill.create(name: "Tort", price: 5000)
+Bill.create(name: "Kino", price: 2500)
+Bill.create(name: "Lunapark", price: 3000)
+Bill.create(name: "Lody", price: 1000)
+puts "Bills created"
